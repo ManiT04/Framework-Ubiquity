969fccf64d052a7c08003178b683c5ac299378ed
Manivone <21902892@etu.unicaen.fr>
2021-02-23 13:35:37 +0100

    crudGroups

---
+++ app/controllers/crud/datas/CrudGroupsDatas.php
@@ -0,0 +1,10 @@
+<?php
+namespace controllers\crud\datas;
+
+use Ubiquity\controllers\crud\CRUDDatas;
+ /**
+  * Class CrudGroupsDatas
+  */
+class CrudGroupsDatas extends CRUDDatas{
+	//use override/implement Methods
+}
---
+++ app/controllers/crud/viewers/CrudGroupsViewer.php
@@ -0,0 +1,10 @@
+<?php
+namespace controllers\crud\viewers;
+
+use Ubiquity\controllers\crud\viewers\ModelViewer;
+ /**
+  * Class CrudGroupsViewer
+  */
+class CrudGroupsViewer extends ModelViewer{
+	//use override/implement Methods
+}
