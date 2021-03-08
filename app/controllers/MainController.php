3dee5ec7e425c4c3325a8f9461f9e55fcb4aff5b
Manivone <21902892@etu.unicaen.fr>
2021-03-08 15:08:27 +0100

    bdd, models, mainController ok, partie myAuth en cours

---
+++ app/controllers/MainController.php
@@ -0,0 +1,16 @@
+<?php
+namespace controllers;
+
+use Ubiquity\attributes\items\router\Route;
+
+ /**
+ * Controller MainController
+ **/
+class MainController extends ControllerBase{
+
+    #[Route('_default',name: 'home')]
+	public function index(){
+		$this->loadDefaultView();
+	}
+
+}
